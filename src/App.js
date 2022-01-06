63344eeaacd8c6ff6dbe50c581425b20ffb32184
ToseefAhmad <ahmadtouseef946@gmail.com>
2022-01-06 20:06:58 +0500

    implement snapshot lib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
+import React, {useRef} from 'react';
 import './App.css';
+import VideoSnapshot from 'video-snapshot';
+import ReactPlayer from 'react-player'
+
 
 function App() {
+  const [imgSrc, setImgSrc] = React.useState('');
+  const [videoSrc, setVideoSrc] = React.useState('');
+  const videoRef = useRef();
+  const fileRef = useRef();
+
+  const handleClick = async (e) => {
+      const snapshoter = new VideoSnapshot(e.target.files[0]);
+      const previewSrc = await snapshoter.takeSnapshot();
+      setVideoSrc(URL.createObjectURL(e.target.files[0]));
+      console.log(e.target.files[0].name)
+      setImgSrc(previewSrc);
+
+
+      // img.src = previewSrc;
+      //
+      // document.body.appendChild(img);
+  }
+
+  const handleTakeSnapShot = async () => {
+      const file = fileRef.current.files[0];
+      console.log(file)
+      const snapshoter = new VideoSnapshot(file);
+      const previewSrc = await snapshoter.takeSnapshot(file.currentTime);
+      // setVideoSrc(URL.createObjectURL(e.target.files[0]));
+      // console.log(e.target.files[0].name)
+      setImgSrc(previewSrc);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ReactPlayer url={videoSrc} playing={true} controls={true} />
+        <input type="file" ref={fileRef} onChange={handleClick}/>
+        <img src={imgSrc} />
+        <button onClick={handleTakeSnapShot}>take snapshot</button>
     </div>
   );
 }
